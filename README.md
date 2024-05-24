85c092094099dbebb3606be411eeb3d021e4079a
Eric Hengber <ehengber@gmail.com>
2024-05-24 01:53:12 -0400

    material ui working

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DM-Website-25
+
+npm install @mui/material @emotion/react @emotion/styled
+npm install @fontsource/roboto
+npm install @mui/icons-material
---
+++ dm-website/src/App.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import AppAppBar from './components/AppAppBar';
+import Footer from './components/Footer';
+import getLPTheme from './getLPTheme';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Highlights from './components/Highlights';
+import Hero from './components/Hero';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
+import LandingPage from './LandingPage';
+
+function App() {
+  const [mode, setMode] = React.useState('dark');
+  const [showCustomTheme, setShowCustomTheme] = React.useState(true);
+  const LPtheme = createTheme(getLPTheme(mode));
+  const defaultTheme = createTheme({ palette: { mode } });
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
+
+  return (
+      <div className="App">
+      <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}> </ThemeProvider>
+      <CssBaseline />
+        <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+        <Box sx={{ bgcolor: 'background.default' }}>
+            <LandingPage/>
+        <Footer/>
+        </Box>        
+      </div>
+  );
+}
+
+export default App;
