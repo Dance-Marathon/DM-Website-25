07b222aa038d63056277c6477a6ed1d28cb94122
ZacharyGrosswirth <zacharygrosswirth1@gmail.com>
2024-10-18 16:39:05 -0400

    landing page partner scrolling bar

---
+++ dm-website/src/components/LandingCarousel.js
@@ -0,0 +1,179 @@
+// MuiCarousel.js
+import React, { useState } from 'react';
+import { Box, Button, Typography } from '@mui/material';
+import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
+
+import Sponsor1 from '../assets/images/sponsors/Sponsor1.png';
+import Sponsor2 from '../assets/images/sponsors/Sponsor2.png';
+import Sponsor3 from '../assets/images/sponsors/Sponsor3.png';
+import Sponsor4 from '../assets/images/sponsors/Sponsor4.png';
+import Sponsor5 from '../assets/images/sponsors/Sponsor5.png';
+import Sponsor6 from '../assets/images/sponsors/Sponsor6.png';
+import Sponsor7 from '../assets/images/sponsors/Sponsor7.png';
+import Sponsor8 from '../assets/images/sponsors/Sponsor8.png';
+import Sponsor9 from '../assets/images/sponsors/Sponsor9.png';
+import Sponsor10 from '../assets/images/sponsors/Sponsor10.png';
+import Sponsor11 from '../assets/images/sponsors/Sponsor11.png';
+import Sponsor12 from '../assets/images/sponsors/Sponsor12.png';
+import Sponsor13 from '../assets/images/sponsors/Sponsor13.png';
+import Sponsor14 from '../assets/images/sponsors/Sponsor14.png';
+import Sponsor15 from '../assets/images/sponsors/Sponsor15.png';
+import Sponsor16 from '../assets/images/sponsors/Sponsor16.png';
+import Sponsor17 from '../assets/images/sponsors/Sponsor17.png';
+import Sponsor18 from '../assets/images/sponsors/Sponsor18.png';
+import Sponsor19 from '../assets/images/sponsors/Sponsor19.png';
+import Sponsor20 from '../assets/images/sponsors/Sponsor20.png';
+import Sponsor21 from '../assets/images/sponsors/Sponsor21.png';
+import Sponsor22 from '../assets/images/sponsors/Sponsor22.png';
+import Sponsor23 from '../assets/images/sponsors/Sponsor23.png';
+import Sponsor24 from '../assets/images/sponsors/Sponsor24.png';
+import Sponsor25 from '../assets/images/sponsors/Sponsor25.png';
+import Sponsor26 from '../assets/images/sponsors/Sponsor26.png';
+import Sponsor27 from '../assets/images/sponsors/Sponsor27.png';
+import Sponsor28 from '../assets/images/sponsors/Sponsor28.png';
+import Sponsor29 from '../assets/images/sponsors/Sponsor29.png';
+import Sponsor30 from '../assets/images/sponsors/Sponsor30.png';
+import Sponsor31 from '../assets/images/sponsors/Sponsor31.png';
+import Sponsor32 from '../assets/images/sponsors/Sponsor32.png';
+import Sponsor33 from '../assets/images/sponsors/Sponsor33.png';
+import Sponsor34 from '../assets/images/sponsors/Sponsor34.png';
+import Sponsor35 from '../assets/images/sponsors/Sponsor35.png';
+import Sponsor36 from '../assets/images/sponsors/Sponsor36.png';
+import Sponsor37 from '../assets/images/sponsors/Sponsor37.png';
+import Sponsor38 from '../assets/images/sponsors/Sponsor38.png';
+import Sponsor39 from '../assets/images/sponsors/Sponsor39.png';
+import Sponsor40 from '../assets/images/sponsors/Sponsor40.png';
+import Sponsor41 from '../assets/images/sponsors/Sponsor41.png';
+import Sponsor42 from '../assets/images/sponsors/Sponsor42.png';
+import Sponsor43 from '../assets/images/sponsors/Sponsor43.png';
+import Sponsor44 from '../assets/images/sponsors/Sponsor44.png';
+import Sponsor45 from '../assets/images/sponsors/Sponsor45.png';
+import Sponsor46 from '../assets/images/sponsors/Sponsor46.png';
+import Sponsor47 from '../assets/images/sponsors/Sponsor47.png';
+import Sponsor48 from '../assets/images/sponsors/Sponsor48.png';
+import Sponsor49 from '../assets/images/sponsors/Sponsor49.png';
+import Sponsor50 from '../assets/images/sponsors/Sponsor50.png';
+import Sponsor51 from '../assets/images/sponsors/Sponsor51.png';
+import Sponsor52 from '../assets/images/sponsors/Sponsor52.png';
+import Sponsor53 from '../assets/images/sponsors/Sponsor53.png';
+import Sponsor54 from '../assets/images/sponsors/Sponsor54.png';
+import Sponsor55 from '../assets/images/sponsors/Sponsor55.png';
+import Sponsor56 from '../assets/images/sponsors/Sponsor56.png';
+import Sponsor57 from '../assets/images/sponsors/Sponsor57.png';
+import Sponsor58 from '../assets/images/sponsors/Sponsor58.png';
+import Sponsor59 from '../assets/images/sponsors/Sponsor59.png';
+import Sponsor60 from '../assets/images/sponsors/Sponsor60.png';
+
+const images = [
+  { src: Sponsor1, alt: 'Sponsor 1' },
+  { src: Sponsor2, alt: 'Sponsor 2' },
+  { src: Sponsor3, alt: 'Sponsor 3' },
+  { src: Sponsor4, alt: 'Sponsor 4' },
+  { src: Sponsor5, alt: 'Sponsor 5' },
+  { src: Sponsor6, alt: 'Sponsor 6' },
+  { src: Sponsor7, alt: 'Sponsor 7' },
+  { src: Sponsor8, alt: 'Sponsor 8' },
+  { src: Sponsor9, alt: 'Sponsor 9' },
+  { src: Sponsor10, alt: 'Sponsor 10' },
+  { src: Sponsor11, alt: 'Sponsor 11' },
+  { src: Sponsor12, alt: 'Sponsor 12' },
+  { src: Sponsor13, alt: 'Sponsor 13' },
+  { src: Sponsor14, alt: 'Sponsor 14' },
+  { src: Sponsor15, alt: 'Sponsor 15' },
+  { src: Sponsor16, alt: 'Sponsor 16' },
+  { src: Sponsor17, alt: 'Sponsor 17' },
+  { src: Sponsor18, alt: 'Sponsor 18' },
+  { src: Sponsor19, alt: 'Sponsor 19' },
+  { src: Sponsor20, alt: 'Sponsor 20' },
+  { src: Sponsor21, alt: 'Sponsor 21' },
+  { src: Sponsor22, alt: 'Sponsor 22' },
+  { src: Sponsor23, alt: 'Sponsor 23' },
+  { src: Sponsor24, alt: 'Sponsor 24' },
+  { src: Sponsor25, alt: 'Sponsor 25' },
+  { src: Sponsor26, alt: 'Sponsor 26' },
+  { src: Sponsor27, alt: 'Sponsor 27' },
+  { src: Sponsor28, alt: 'Sponsor 28' },
+  { src: Sponsor29, alt: 'Sponsor 29' },
+  { src: Sponsor30, alt: 'Sponsor 30' },
+  { src: Sponsor31, alt: 'Sponsor 31' },
+  { src: Sponsor32, alt: 'Sponsor 32' },
+  { src: Sponsor33, alt: 'Sponsor 33' },
+  { src: Sponsor34, alt: 'Sponsor 34' },
+  { src: Sponsor35, alt: 'Sponsor 35' },
+  { src: Sponsor36, alt: 'Sponsor 36' },
+  { src: Sponsor37, alt: 'Sponsor 37' },
+  { src: Sponsor38, alt: 'Sponsor 38' },
+  { src: Sponsor39, alt: 'Sponsor 39' },
+  { src: Sponsor40, alt: 'Sponsor 40' },
+  { src: Sponsor41, alt: 'Sponsor 41' },
+  { src: Sponsor42, alt: 'Sponsor 42' },
+  { src: Sponsor43, alt: 'Sponsor 43' },
+  { src: Sponsor44, alt: 'Sponsor 44' },
+  { src: Sponsor45, alt: 'Sponsor 45' },
+  { src: Sponsor46, alt: 'Sponsor 46' },
+  { src: Sponsor47, alt: 'Sponsor 47' },
+  { src: Sponsor48, alt: 'Sponsor 48' },
+  { src: Sponsor49, alt: 'Sponsor 49' },
+  { src: Sponsor50, alt: 'Sponsor 50' },
+  { src: Sponsor51, alt: 'Sponsor 51' },
+  { src: Sponsor52, alt: 'Sponsor 52' },
+  { src: Sponsor53, alt: 'Sponsor 53' },
+  { src: Sponsor54, alt: 'Sponsor 54' },
+  { src: Sponsor55, alt: 'Sponsor 55' },
+  { src: Sponsor56, alt: 'Sponsor 56' },
+  { src: Sponsor57, alt: 'Sponsor 57' },
+  { src: Sponsor58, alt: 'Sponsor 58' },
+  { src: Sponsor59, alt: 'Sponsor 59' },
+  { src: Sponsor60, alt: 'Sponsor 60' },
+];
+  
+const LandingCarousel = () => {
+    return (
+      <Box
+        sx={{
+          overflow: 'hidden',
+          whiteSpace: 'nowrap',
+          width: '100%',
+          mt: 4,
+          position: 'relative',
+          marginBottom: 4,
+        }}
+      >
+        <Box
+          sx={{
+            display: 'inline-flex',
+            animation: 'scroll 30s linear infinite'
+          }}
+        >
+          {/* Loop images twice for seamless scrolling */}
+          {images.concat(images).map((image, index) => (
+            <Box
+              component="img"
+              key={index}
+              src={image.src}
+              alt={image.alt}
+              sx={{
+                width: '100%', // 5 images at a time
+                height: 200,
+                borderRadius: '8px',
+                marginRight: '15px',
+                marginLeft: '15px',
+              }}
+            />
+          ))}
+        </Box>
+  
+        {/* Animation Keyframes */}
+        <style>
+          {`
+            @keyframes scroll {
+              0% { transform: translateX(0); }
+              100% { transform: translateX(-50%); }
+            }
+          `}
+        </style>
+      </Box>
+    );
+};
+
+export default LandingCarousel;
